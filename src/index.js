df04141036fa93f3fcf71ab8df5c884587557b6f
Tatyana Levchenko <grib.tanya06@mail.ru>
2021-08-20 11:44:00 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function reverse (n) {
-  
+    n = Math.abs(n)
+    a=n.toString().split('').reverse().join('')
+    return a;
+
+    /* n>=0*/
+    /*? +(`${n}`.split('').reverse().join(''))
+    : -(`${n}`.split('').reverse().join(''))
+    return n;
+    */
+
+    /*let n1 = 0;
+        for ( n; n; n = Math.floor(n/10)){
+            n1 *= 10;
+            n1 += n % 10;
+        }  
+        console.log(n1);
+        */
 }
